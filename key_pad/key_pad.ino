13f9e8d2924de57e4d892fe51d18ab90c1e397ba
Tiago <ottiagos@gmail.com>
2023-07-09 23:54:53 -0300

    basic keypad serial logs key pressed

---
+++ key_pad/key_pad.ino
@@ -0,0 +1,30 @@
+#include <Keypad.h>
+// define the symbols on the buttons of the keypad
+
+char keys[4][4] = {
+ {'1', '2', '3', 'A'},
+ {'7', '8', '9', 'C'},
+ {'4', '5', '6', 'B'},
+ {'*', '0', '#', 'D'}
+};
+
+byte rowPins[4] = {11, 10, 9, 8}; // connect to the row pinouts of the keypad
+byte colPins[4] = {7, 6, 5, 4}; // connect to the column pinouts of the keypad
+
+// initialize an instance of class Keypad
+Keypad myKeypad = Keypad(makeKeymap(keys), rowPins, colPins, 4, 4);
+
+void setup() {
+  Serial.begin(9600); // Initialize the serial port and set the baud rate to 9600
+}
+
+void loop() {
+  // Get the character input
+  char keyPressed = myKeypad.getKey();
+
+  // If there is a character input, sent it to the serial port
+  if (keyPressed) {
+    Serial.println(keyPressed);
+  }
+}
+
