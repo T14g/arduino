0ac933c851b4329f2cbc4c0e692b73b46347adf5
Tiago <ottiagos@gmail.com>
2023-07-16 23:40:28 -0300

    added lcd to keypad project

--- key_pad/key_pad.ino
+++ key_pad/key_pad.ino
@@ -1,5 +1,18 @@
+#include <Wire.h>
 #include <Keypad.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
 // define the symbols on the buttons of the keypad
+
+// Set the LCD address
+const int I2C_ADDR = 0x27;  // Replace with your LCD's address
+
+// Set the LCD dimensions (columns x rows)
+const int LCD_COLS = 16;
+const int LCD_ROWS = 2;
+
+// Create LCD object
+hd44780_I2Cexp lcd;
 
 char keys[4][4] = {
  {'1', '2', '3', 'A'},
@@ -16,6 +29,10 @@ Keypad myKeypad = Keypad(makeKeymap(keys), rowPins, colPins, 4, 4);
 
 void setup() {
   Serial.begin(9600); // Initialize the serial port and set the baud rate to 9600
+  lcd.begin(LCD_COLS, LCD_ROWS);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Chose a Exercise");
 }
 
 void loop() {
