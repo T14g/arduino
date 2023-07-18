dc58e24d5ee9614213a139a8db95367308e04201
Tiago <ottiagos@gmail.com>
2023-07-17 21:19:48 -0300

    keypad numbers are now concatenated into a string, list of exercises added

--- key_pad/key_pad.ino
+++ key_pad/key_pad.ino
@@ -16,8 +16,8 @@ hd44780_I2Cexp lcd;
 
 char keys[4][4] = {
  {'1', '2', '3', 'A'},
- {'7', '8', '9', 'C'},
  {'4', '5', '6', 'B'},
+ {'7', '8', '9', 'C'},
  {'*', '0', '#', 'D'}
 };
 
@@ -26,6 +26,29 @@ byte colPins[4] = {7, 6, 5, 4}; // connect to the column pinouts of the keypad
 
 // initialize an instance of class Keypad
 Keypad myKeypad = Keypad(makeKeymap(keys), rowPins, colPins, 4, 4);
+
+String enteredNumber = "";
+
+const char* exerciseList[] = {
+  "Chose an Exercise",
+  "Kb Sumo Squats",
+  "Kb Squats",
+  "Kb Deadlift",
+  "Kb Pushup",
+  "Pushup",
+  "Biceps Kb Hammer",
+  "Concentr Curl Kb",
+  "Kb Biceps Curls",
+  "Kb French Press",
+  "Shoulder Press",
+  "Kb upright row",
+  "Kb Front raise",
+  "Blue B lateral",
+  "Blk B lateral",
+  "Kb ab rota 40s",
+  "Band Abs",
+  "Band Tri pulld"
+};
 
 void setup() {
   Serial.begin(9600); // Initialize the serial port and set the baud rate to 9600
@@ -40,8 +63,24 @@ void loop() {
   char keyPressed = myKeypad.getKey();
 
   // If there is a character input, sent it to the serial port
-  if (keyPressed) {
+  if (keyPressed && isdigit(keyPressed)) {
+    enteredNumber += keyPressed;
     Serial.println(keyPressed);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(exerciseList[enteredNumber.toInt()]);
+    lcd.setCursor(0, 1);
+    lcd.print(enteredNumber);
+  }else{
+    if(keyPressed == 'C') {
+      enteredNumber = "";
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Chose a Exercise");
+      lcd.setCursor(0, 1);
+      lcd.print(enteredNumber);
+    }
   }
 }
 
